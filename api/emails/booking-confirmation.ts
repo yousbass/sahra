1f97a55ad5aaa7517b65e36105db62631da82622
Yusuf Bassam Alhamad <yusuf@Yusufs-MacBook-Pro-8.local>
2025-12-03 13:13:05 +0300

    Update UI / fix features

---
+++ api/emails/booking-confirmation.ts
@@ -0,0 +1,5 @@
+// Explicit route for booking confirmation email (maps to dynamic handler)
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const handler = require('./[action]').default;
+
+export default handler;
